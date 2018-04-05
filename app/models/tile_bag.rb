19c88543d979a3748fba14f0e992fa19f9f084e9
Charles Ellis <hamled@hamled.name>
2018-04-05 16:32:18 -0700

    Style changes for Rubocop compliance

--- app/models/tile_bag.rb
+++ app/models/tile_bag.rb
@@ -1,5 +1,6 @@
-class TileBag < ApplicationRecord
+# frozen_string_literal: true
 
+class TileBag < ApplicationRecord
   # Validations
   validate :tiles_must_contain_only_letters
 
@@ -8,8 +9,8 @@ class TileBag < ApplicationRecord
     'G' => 3, 'H' => 2, 'I' => 9, 'J' => 1, 'K' =>  1, 'L' => 4,
     'M' => 2, 'N' => 6, 'O' => 8, 'P' => 2, 'Q' =>  1, 'R' => 6,
     'S' => 4, 'T' => 6, 'U' => 4, 'V' => 2, 'W' =>  2, 'X' => 1,
-    'Y' => 2, 'Z' => 1,
-  }
+    'Y' => 2, 'Z' => 1
+  }.freeze
 
   # Ensure that new tile bags start with the full set of tiles
   before_create do
@@ -21,8 +22,8 @@ class TileBag < ApplicationRecord
   private
 
   def tiles_must_contain_only_letters
-    if /[^A-Z]/.match(tiles)
-      errors.add(:tiles, :invalid, message: "must contain only letters")
+    if /[^A-Z]/.match? tiles
+      errors.add(:tiles, :invalid, message: 'must contain only letters')
     end
   end
 end
