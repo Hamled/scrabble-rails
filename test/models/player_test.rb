3bfb85e3329e7f3cfcf4603878634d408361f790
Charles Ellis <hamled@hamled.name>
2018-04-06 15:24:01 -0700

    Player must have a name

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -22,5 +22,10 @@ describe Player do
     it "must be valid with a full tile rack" do
       value(full_player).must_be :valid?
     end
+
+    it "must be invalid without a name" do
+      player.name = nil
+      value(player).wont_be :valid?
+    end
   end
 end
