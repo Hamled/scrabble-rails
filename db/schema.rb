ed95ba33375e693403e283c6d9f0002dc62faad0
Charles Ellis <hamled@hamled.name>
2018-04-06 14:40:34 -0700

    Player model
    This model represents a single player within a game of Scrabble. Players
    will eventually have a set of turns that they have played, and will all
    share the same tile bag.
    
    For now, players have a name and a tile rack of letters they can play.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405212454) do
+ActiveRecord::Schema.define(version: 20180406213201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "tile_rack", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tile_bags", force: :cascade do |t|
     t.string "tiles", default: "", null: false
---
+++ test/models/player_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+describe Player do
+  let(:player) { players(:empty_rack) }
+  let(:full_player) { players(:full_rack) }
+
+  describe "attributes" do
+    it "must have a name" do
+      value(player).must_respond_to :name
+    end
+
+    it "must have a tile rack" do
+      value(player).must_respond_to :tile_rack
+    end
+  end
+
+  describe "validations" do
+    it "must be valid with an empty tile rack" do
+      value(player).must_be :valid?
+    end
+
+    it "must be valid with a full tile rack" do
+      value(full_player).must_be :valid?
+    end
+  end
+end
