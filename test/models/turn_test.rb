8e3a322de3880b5478fd616240924b30f9f06879
Charles Ellis <hamled@hamled.name>
2018-04-09 14:02:05 -0700

    Turn#score method for calculating score for turn
    The tests for this, and thus the method logic, are lifted directly from
    the original Scrabble project.

--- test/models/turn_test.rb
+++ test/models/turn_test.rb
@@ -33,4 +33,43 @@ describe Turn do
       value(turn).wont_be :valid?
     end
   end
+
+  describe "#score" do
+    # Helper method to generate a new turn with given word
+    def turn(word)
+      Turn.new(player: players(:angie), word: word)
+    end
+
+    it "correctly scores simple words" do
+      WORD_SCORES = {
+        'DOG' => 5,
+        'CAT' => 5,
+        'PIG' => 6
+      }
+
+      WORD_SCORES.each do |word, score|
+        value(turn(word).score).must_equal score
+      end
+    end
+
+    it "adds 50 points for a 7-letter word" do
+      value(turn('ACADEMY').score).must_equal 65
+    end
+
+    it "returns nil for strings containing bad characters" do
+      BAD_CHAR_WORDS = ['#$%^', 'CHAR^', ' ']
+
+      BAD_CHAR_WORDS.each do |word|
+        value(turn(word).score).must_be_nil
+      end
+    end
+
+    it "returns nil for words > 7 letters" do
+      value(turn('abcdefgh').score).must_be_nil
+    end
+
+    it "returns nil with an empty word" do
+      value(turn('').score).must_be_nil
+    end
+  end
 end
