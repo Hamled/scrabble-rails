e510b6f83fa299a28047a12c1f86cb78c7d9b84d
Charles Ellis <hamled@hamled.name>
2018-04-08 18:26:42 -0700

    Played words can only be 1-7 letters

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,3 +1,5 @@
 class Turn < ApplicationRecord
   belongs_to :player
+
+  validates :word, length: { minimum: 1, maximum: 7 }
 end
--- test/models/turn_test.rb
+++ test/models/turn_test.rb
@@ -17,5 +17,20 @@ describe Turn do
     it "must be valid with a valid word" do
       value(turn).must_be :valid?
     end
+
+    it "must be invalid with no word" do
+      turn.word = nil
+      value(turn).wont_be :valid?
+
+      turn.word = ''
+      value(turn).wont_be :valid?
+    end
+
+    it "must have a maximum of 7 letters" do
+      turn.word += 'S'
+      value(turn.word.length).must_be :>, 7 # Sanity check
+
+      value(turn).wont_be :valid?
+    end
   end
 end
