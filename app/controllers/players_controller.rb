f5e9abf535d7be89c53b36802d3de0e6af505e8a
Charles Ellis <hamled@hamled.name>
2018-04-12 09:57:47 -0700

    Players can play words from details page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,19 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:player_id] || params[:id])
   end
+
+  def play
+    player = Player.find(params[:player_id] || params[:id])
+    tile_bag = TileBag.last
+
+
+    # This may fail if word cannot be played, so we use
+    # ensure to always redirect to the player page
+    ApplicationRecord.transaction do
+      player.play!(params[:word])
+      player.draw_tiles!(tile_bag)
+    end
+  ensure
+    redirect_to player
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :players, only: [:show]
+  resources :players, only: [:show] do
+    post :play
+  end
 end
